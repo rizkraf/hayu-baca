fdb5c55187e1f5f83cf14a29764bd2684bdfd07b
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-03-30 22:54:33 +0700

    init project starter

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
