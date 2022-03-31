1110b7add5b172a4e572a7acf8fd87ea59ffe577
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-03-31 18:28:58 +0700

    Add navbar, pagination, change some basic style - note: sorry baru segini;')

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div v-if="!guest">
-        <img :src="user.photo_profile ? apiDomain + user.photo_profile : 'https://www.gravatar.com/avatar/Guest?d=identicon&f=y'"> | {{ user.name }}
+      <div>
+        <b-navbar toggleable="lg" type="light" variant="light">
+          <b-navbar-brand href="#">HayuBaca!</b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item href="#"><router-link to="/">Home</router-link></b-nav-item>
+              <b-nav-item href="#"><router-link to="/blogs">Blogs</router-link></b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
       </div>
-      <div v-if="guest">
-        <img src="https://www.gravatar.com/avatar/Guest?d=identicon&f=y"> | User Guest
-      </div>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/blogs">Blogs</router-link> |
-      <span v-if="guest"><router-link to="/login">Login</router-link> | <router-link to="/signup">Sign Up</router-link></span><span v-else><button v-on:click="logout">Logout</button></span>
+
+      <div v-if="guest"><img src="https://www.gravatar.com/avatar/Guest?d=identicon&f=y" /> | User Guest</div>
+      | <span v-if="guest"><router-link to="/login">Login</router-link> | <router-link to="/signup">Sign Up</router-link></span
+      ><span v-else><button v-on:click="logout">Logout</button></span>
     </div>
-    <router-view/>
+
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "times new roman";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
+  height: 180vh;
+  background-color: #ecf0f1;
 }
 
 #nav a {
@@ -39,48 +59,48 @@
 </style>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 export default {
-  data : () => ({
-    apiDomain : 'https://demo-api-vue.sanbercloud.com'
+  data: () => ({
+    apiDomain: "https://demo-api-vue.sanbercloud.com",
   }),
-  computed : {
-    ...mapGetters ({
-      guest : 'auth/guest',
-      user : 'auth/user',
-      token : 'auth/token'
-    })
+  computed: {
+    ...mapGetters({
+      guest: "auth/guest",
+      user: "auth/user",
+      token: "auth/token",
+    }),
   },
-  methods : {
+  methods: {
     logout() {
       let config = {
-        method : 'post',
-        url : this.apiDomain + '/api/v2/auth/logout',
-        headers : {
-          'Authorization' : 'Bearer ' + this.token
-        }
-      }
+        method: "post",
+        url: this.apiDomain + "/api/v2/auth/logout",
+        headers: {
+          Authorization: "Bearer " + this.token,
+        },
+      };
 
       this.axios(config)
         .then(() => {
-          this.setToken('')
-          this.setUser({})
+          this.setToken("");
+          this.setUser({});
           //this.guest = true
         })
         .catch((response) => {
-          console.log(response.data.error)
-        })
+          console.log(response.data.error);
+        });
     },
     ...mapActions({
-      setToken : 'auth/setToken',
-      setUser : 'auth/setUser',
-      checkToken : 'auth/checkToken'
-    })
+      setToken: "auth/setToken",
+      setUser: "auth/setUser",
+      checkToken: "auth/checkToken",
+    }),
   },
-  mounted(){
-    if(this.token){
-      this.checkToken(this.token)
+  mounted() {
+    if (this.token) {
+      this.checkToken(this.token);
     }
-  }
-}
+  },
+};
 </script>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,10 +1,30 @@
 <template>
-    <div v-if="blog.id">
-        <img :src="blog.photo ? apiDomain + blog.photo : 'https://picsum.photos/200/300'"><br>
-        Title : {{ blog.title }} <br>
-        Description : {{ blog.description }}<hr>
+    <div v-if="blog.id" class="contentBlog p-5">
+        <img :src="blog.photo ? apiDomain + blog.photo : 'https://picsum.photos/200/300'" class="rounded"><br>
+        <tabel border="1">
+            <tr>
+                <td><h3>Title</h3></td>
+                <td><h3>:</h3></td>
+                <td><h3>{{ blog.title }}</h3></td>
+            </tr>
+            <tr>
+                <td><h3>Description</h3></td>
+                <td><h3>:</h3></td>
+                <td><h3>{{ blog.description }}</h3></td>
+            </tr>
+            <tr>
+            </tr>
+        </tabel>
     </div>
 </template>
+
+<style>
+    img {
+        margin-bottom: 20px;
+        max-width: 350px;
+        max-height: 350px;
+    }
+</style>
 
 <script>
 // Disini Cuma Ambil Blog Yang Ingin Di Ambil...
