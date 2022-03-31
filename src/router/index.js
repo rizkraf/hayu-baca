c163379ae62921659cecd34beb498c262277d92b
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-03-31 20:38:04 +0700

    menambahkan halaman post crud

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
+    path: "/",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
   },
   {
-    path: '/blogs',
-    name: 'Blogs',
-    component: () => import('../views/Blogs.vue')
+    path: "/blogs",
+    name: "Blogs",
+    component: () => import("../views/Blogs.vue"),
   },
   {
-    path: '/blog/:id',
-    name: 'Blog',
-    component: () => import('../views/Blog.vue')
+    path: "/blog/:id",
+    name: "Blog",
+    component: () => import("../views/Blog.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
   },
   {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import('../views/Signup.vue')
-  }
-]
+    path: "/signup",
+    name: "Signup",
+    component: () => import("../views/Signup.vue"),
+  },
+  {
+    path: "/post",
+    name: "Post",
+    component: () => import("../views/Post.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
