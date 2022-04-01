eca115580b4aa214adf8d789a99685557e4204ea
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-04-01 14:30:17 +0700

    fix carousel, add container, fix navbar and header button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="home">
+  <b-container>
     <div class="p-2 mb-5">
       <h1>Selamat Membaca di <span>HayuBaca!</span></h1>
-      <h3><span>HayuBaca!</span> merupakan platform digital yang menampung para penjelajah internet untuk bertukar dan berbagi informasi menarik seputar dunia secara gratis!</h3><br>
-      <h5>Bergabunglah bersama kami untuk mendaptakan berita terpercaya dan terbaru.</h5>
-      <b-button pill variant="success" href="#signup.vue">Mendaftar <b-icon icon="arrow-right-circle"></b-icon></b-button>
+      <h3><span>HayuBaca!</span> merupakan platform digital yang menampung para penjelajah internet untuk bertukar dan
+        berbagi informasi menarik seputar dunia secara gratis!</h3><br>
+      <div>
+        <h5>Bergabunglah bersama kami untuk mendaptakan berita terpercaya dan terbaru.</h5>
+        <b-button v-if="guest" pill variant="success" to="/signup">Daftar <b-icon icon="arrow-right-circle"></b-icon>
+        </b-button>
+      </div>
     </div>
-    <div>
-    <b-carousel
+    <b-row class="d-flex justify-content-center">
+      <b-col cols="8">
+        <b-carousel id="carousel-fade" 
+        fade 
+        indicators 
+        controls 
+          style="text-shadow: 1px 1px 2px #000; margin: auto;" 
+          @sliding-start="onSlideStart" 
+          @sliding-end="onSlideEnd">
+          <b-carousel-slide v-for="blog in blogs" 
+          :key="`blog-` + blog.id" 
+          :blog="blog" 
+          :caption="blog.title"
+          :text="`${blog.description.substring(0, 100)} ...`"
+          :img-src="blog.photo ? apiDomain + blog.photo : 'https://picsum.photos/200/300'">
+            <b-link class="stretched-link" :to="'/blog/'+blog.id"></b-link>
+          </b-carousel-slide>
+        </b-carousel>
+      </b-col>
 
-      style="text-shadow: 1px 1px 2px #333; width: 800px; margin: auto;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <b-carousel-slide
-      v-for="blog in blogs"
-      :key="`blog-` + blog.id"
-      :blog="blog"
-      :caption="blog.title"
-      :text="blog.description "
-      :img-src="blog.photo ? apiDomain + blog.photo : 'https://picsum.photos/200/300'"
-      ></b-carousel-slide>
-    </b-carousel>
-  </div>
+    </b-row>
 
-  </div>
+  </b-container>
 </template>
 
 <style>
   span {
     font-weight: 900;
     text-shadow: 1px 1px 2px #333;
-  }
-  h3{
-    margin: 10px 250px;
   }
 </style>
 
@@ -44,6 +49,7 @@ export default {
   data: () => ({
     apiDomain : 'https://demo-api-vue.sanbercloud.com',
     blogs : [],
+    guest: true,
     slide: 0,
     sliding: null
   }),
