1512608954153b11b9f5c388dc42d3d23839bb3e
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-04-01 10:16:13 +0700

    Add some styling

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,18 @@
 
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-nav-item href="#" class="mr-2 mt-1"><router-link to="/"><b-icon icon="house-door-fill"></b-icon><h6> Beranda</h6></router-link></b-nav-item>
-              <b-nav-item href="#" class="mr-2 mt-1"><router-link to="/blogs"><b-icon icon="file-earmark-fill"></b-icon><h6> Blogs</h6></router-link></b-nav-item>
+              <b-nav-item href="#" class="mr-2 mt-1"
+                ><router-link to="/"
+                  ><b-icon icon="house-door-fill"></b-icon>
+                  <h6>Beranda</h6></router-link
+                ></b-nav-item
+              >
+              <b-nav-item href="#" class="mr-2 mt-1"
+                ><router-link to="/blogs"
+                  ><b-icon icon="file-earmark-fill"></b-icon>
+                  <h6>Blogs</h6></router-link
+                ></b-nav-item
+              >
             </b-navbar-nav>
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
@@ -27,8 +37,7 @@
         </b-navbar>
       </div>
 
-      <div v-if="!guest">
-      <img :src="user.photo_profile ? apiDomain + user.photo_profile : 'https://www.gravatar.com/avatar/Guest?d=identicon&f=y'" style="width: 50px; height: 50px" /> | {{ user.name }}</div>
+      <div v-if="!guest"><img :src="user.photo_profile ? apiDomain + user.photo_profile : 'https://www.gravatar.com/avatar/Guest?d=identicon&f=y'" style="width: 50px; height: 50px" /> | {{ user.name }}</div>
       <div v-if="guest"><img src="https://www.gravatar.com/avatar/Guest?d=identicon&f=y" style="width: 50px; height: 50px" /> | Pengguna Tamu</div>
       <span v-if="guest"><router-link to="/login">Masuk</router-link> | <router-link to="/signup">Mendaftar</router-link></span
       ><span v-else><button v-on:click="logout">Keluar</button></span>
@@ -44,7 +53,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding-Bottom: 50px;
+  padding-bottom: 50px;
   background-image: url("./assets/background.png");
   background-size: cover;
   background-repeat: no-repeat;
@@ -61,13 +70,13 @@
 img {
   max-width: 350px;
   max-height: 350px;
-border-radius: 60px;
-border-top-left-radius: 10px;
-border-bottom-right-radius: 10px;
+  border-radius: 60px;
+  border-top-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 }
 .title {
   font-weight: bold;
-text-shadow: 2px 2px 4px #333;
+  text-shadow: 2px 2px 4px #333;
 }
 h6 {
   font-weight: bold;
