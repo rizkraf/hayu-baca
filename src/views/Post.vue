a0609d6c993e3f2ef61f83b52fd320ba922b94dd
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-04-02 09:54:04 +0700

    fix double code

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -46,77 +46,6 @@
     </table>
     </div>
   </b-container>
-    <b-container>
-        <!-- Nanti munculnya cuman ketika udah login doang -->
-        <b-form id="form" @submit.prevent="submitForm()">
-            <b-alert :show="dismissCountDown"
-            dismissible
-            fade
-            :variant="alertVariant"
-            @dismiss-count-down="countDownChanged">{{alertText}}
-            </b-alert>
-            <label for="judul">Judul</label>
-            <b-form-input 
-            name="title"
-            ref="title"
-            :state="titleError"
-            v-model="title"
-            placeholder="Masukan Judul ..." 
-            :readonly="buttonStatus == 'upload'"
-            id="judul"></b-form-input>
-
-            <label for="deskripsi">Isi Artikel</label>
-            <b-form-textarea
-            id="textarea"
-            v-model="description"
-            ref="description"
-            :state="descriptionError"
-            placeholder="Masukan Deskripsi ..."
-            rows="3"
-            :readonly="buttonStatus == 'upload'"
-            ></b-form-textarea>
-
-            <div v-if="buttonStatus == 'submit'">
-                <b-button pill type="submit" variant="success" class="mt-3">Terbitkan</b-button>
-            </div>
-
-            <div v-else-if="buttonStatus == 'update'">
-                <b-button pill @click="updateBlog(idEditBlog)" variant="success" class="mt-3">Ubah</b-button>
-                <b-button pill @click="clearForm" variant="danger" class="mt-3 ml-3">Batal</b-button>
-            </div>
-
-            <div v-else-if="buttonStatus == 'upload'">
-                <b-form-file class="mt-3"
-                name="photo" ref="photo"
-                placeholder="Masukkan gambar disini"
-                drop-placeholder="Masukkan gambar disini..."
-                ></b-form-file>
-                <b-button pill @click="submitPhoto(idUploadBlog)" variant="success" class="mt-3">Unggah</b-button>
-                <b-button pill @click="clearForm" variant="danger" class="mt-3 ml-3">Batal</b-button>
-            </div>
-
-        </b-form>
-
-        <br><br>
-
-        <table border=1 class="tableArtikel">
-            <tr v-for="blog of blogs" :key="`blog-` + blog.id">
-                <td>
-                    <img width=100 :src="blog.photo ? domain + blog.photo : 'https://dummyimage.com/16:9x1080'" alt="">
-                </td>
-                <td>
-                    <b>Judul : </b> {{ blog.title }} <br>
-                    <b>Deskripsi : </b> {{ blog.description }} <br>
-
-                </td>
-                <td class="d-flex">
-                    <b-button pill class="ml-1 mt-1" variant="outline-info" @click="editBlog(blog)">Ubah</b-button>
-                    <b-button pill class="ml-1 mt-1" variant="outline-danger" @click="removeBlog(blog.id)">Hapus</b-button>
-                    <b-button pill class="ml-1 mt-1" variant="outline-secondary" @click="uploadPhoto(blog)">Unggah Foto</b-button>
-                </td>
-            </tr>
-        </table>
-    </b-container>
 </template>
 
 <!-- ini styling untuk Post. -->
@@ -171,37 +100,6 @@
 
 <script>
 export default {
-
-  data: () => ({
-    title: "",
-    description: "",
-    blogs: [],
-    domain: "https://demo-api-vue.sanbercloud.com/",
-    buttonStatus: "submit",
-    idEditBlog: null,
-    idUploadBlog: null,
-    titleError: null,
-    descriptionError: null,
-    dismissSecs: 5,
-    dismissCountDown: 0,
-    alertVariant: "success",
-    alertText: "Artikel telah berhasil diterbitkan",
-  }),
-
-  methods: {
-
-    // Form Submit
-    validationForm: function () {
-      if (this.title.length < 5) {
-        this.titleError = false;
-        this.$refs.title.focus();
-      }
-
-      if (this.description.length < 5) {
-        this.descriptionError = false;
-        this.$refs.description.focus();
-      }
-    },
     data: () => ({
         title: '',
         description: '',
@@ -217,43 +115,34 @@ export default {
         alertVariant: 'success',
         alertText: 'Artikel telah berhasil diterbitkan'
     }),
-
     methods: {
         validationForm: function () {
-
             if (this.title.length < 5) {
                 this.titleError = false
                 this.$refs.title.focus()
             }
-
             if (this.description.length < 5) {
                 this.descriptionError = false
                 this.$refs.description.focus()
             }
         },
-
         countDownChanged(dismissCountDown) {
             this.dismissCountDown = dismissCountDown
         },
         showAlert() {
             this.dismissCountDown = this.dismissSecs
         },
-
         submitForm: function () {
-
             this.validationForm()
-
             if (this.titleError === null && this.descriptionError === null) {
                 let formData = new FormData()
                 formData.append('title', this.title)
                 formData.append('description', this.description)
-
                 const config = {
                     method: "post",
                     url: `${this.domain}/api/blog`,
                     data: formData
                 }
-
                 this.axios(config)
                     .then((response) => {
                         this.clearForm()
@@ -266,13 +155,11 @@ export default {
                     })
             }
         },
-
         getBlogs: function () {
             const config = {
                 method: "get",
                 url: `${this.domain}/api/blog`,
             }
-
             this.axios(config)
                 .then((response) => {
                     this.blogs = response.data.blogs
@@ -281,18 +168,14 @@ export default {
                     console.log(error)
                 })
         },
-
         removeBlog: function (id) {
-
             const config = {
                 method: "post",
                 url: `${this.domain}/api/blog/${id}`,
                 params: {
                     _method: "DELETE"
                 }
-
             }
-
             this.axios(config)
                 .then((response) => {
                     this.getBlogs()
@@ -305,10 +188,8 @@ export default {
                 .catch((error) => {
                     console.log(error)
                 })
-
             console.log(id)
         },
-
         editBlog: function (blog) {
             window.scrollTo(0, 0)
             this.title = blog.title
@@ -316,7 +197,6 @@ export default {
             this.buttonStatus = 'update'
             this.idEditBlog = blog.id
         },
-
         clearForm: function () {
             this.title = ''
             this.description = ''
@@ -324,17 +204,12 @@ export default {
             this.idEditBlog = null
             this.idUploadBlog = null
         },
-
         updateBlog: function (id) {
-
             this.validationForm()
-
             if (this.titleError === null && this.descriptionError === null) {
-
                 let formData = new FormData()
                 formData.append('title', this.title)
                 formData.append('description', this.description)
-
                 let config = {
                     method: "post",
                     url: `${this.domain}/api/blog/${id}`,
@@ -343,7 +218,6 @@ export default {
                     },
                     data: formData
                 }
-
                 this.axios(config)
                     .then((response) => {
                         this.clearForm()
@@ -351,168 +225,43 @@ export default {
                         this.alertText = 'Artikel telah berhasil diubah'
                         this.showAlert()
                         response.data.message
-
-    countDownChanged(dismissCountDown) {
-      this.dismissCountDown = dismissCountDown;
-    },
-    showAlert() {
-      this.dismissCountDown = this.dismissSecs;
-    },
-
-    submitForm: function () {
-      this.validationForm();
-
-      if (this.titleError === null && this.descriptionError === null) {
-        let formData = new FormData();
-        formData.append("title", this.title);
-        formData.append("description", this.description);
-
-        const config = {
-          method: "post",
-          url: `${this.domain}/api/blog`,
-          data: formData,
-        };
-
-        this.axios(config)
-          .then((response) => {
-            this.clearForm();
-            this.getBlogs();
-            this.showAlert();
-            response.data.message;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    },
-
-    getBlogs: function () {
-      const config = {
-        method: "get",
-        url: `${this.domain}/api/blog`,
-      };
-
-      this.axios(config)
-        .then((response) => {
-          this.blogs = response.data.blogs;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    // Delete Blog Function
-    removeBlog: function (id) {
-      const config = {
-        method: "post",
-        url: `${this.domain}/api/blog/${id}`,
-        params: {
-          _method: "DELETE",
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            }
+        },
         uploadPhoto: function (blog) {
             window.scrollTo(0, 0)
             this.title = blog.title
             this.description = blog.description
             this.buttonStatus = 'upload'
             this.idUploadBlog = blog.id
-
         },
-      };
-
-      this.axios(config)
-        .then((response) => {
-          this.getBlogs();
-          this.alertText = "Artikel telah berhasil dihapus";
-          this.alertVariant = "danger";
-          this.showAlert();
-          response.data.message;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      console.log(id);
+        submitPhoto: function (id) {
+            let file = this.$refs.photo.files[0]
+            let formData = new FormData()
+            formData.append('photo', file)
+            let config = {
+                method: "post",
+                url: `${this.domain}/api/blog/${id}/upload-photo`,
+                data: formData
+            }
+            this.axios(config)
+                .then((response) => {
+                    this.clearForm()
+                    this.getBlogs()
+                    this.alertText = 'Foto telah berhasil ditambahkan'
+                    this.showAlert()
+                    response.data.message
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
     },
-    // Edit Blog Function
-    editBlog: function (blog) {
-      this.title = blog.title;
-      this.description = blog.description;
-      this.buttonStatus = "update";
-      this.idEditBlog = blog.id;
-    },
-
-    clearForm: function () {
-      this.title = "";
-      this.description = "";
-      this.buttonStatus = "submit";
-      this.idEditBlog = null;
-      this.idUploadBlog = null;
-    },
-    // Update Blog Function
-    updateBlog: function (id) {
-      this.validationForm();
-
-      if (this.titleError === null && this.descriptionError === null) {
-        let formData = new FormData();
-        formData.append("title", this.title);
-        formData.append("description", this.description);
-
-        let config = {
-          method: "post",
-          url: `${this.domain}/api/blog/${id}`,
-          params: {
-            _method: "PUT",
-          },
-          data: formData,
-        };
-
-        this.axios(config)
-          .then((response) => {
-            this.clearForm();
-            this.getBlogs();
-            this.alertText = "Artikel telah berhasil diubah";
-            this.showAlert();
-            response.data.message;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    },
-    // Upload photo Blog Function
-    uploadPhoto: function (blog) {
-      this.title = blog.title;
-      this.description = blog.description;
-      this.buttonStatus = "upload";
-      this.idUploadBlog = blog.id;
-    },
-
-    submitPhoto: function (id) {
-      let file = this.$refs.photo.files[0];
-
-      let formData = new FormData();
-      formData.append("photo", file);
-
-      let config = {
-        method: "post",
-        url: `${this.domain}/api/blog/${id}/upload-photo`,
-        data: formData,
-      };
-
-      this.axios(config)
-        .then((response) => {
-          this.clearForm();
-
-          this.getBlogs();
-          this.alertText = "Foto telah berhasil ditambahkan";
-          this.showAlert();
-          response.data.message;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
-  created() {
-    this.getBlogs();
-  },
-};
+    created() {
+        this.getBlogs()
+    }
+}
 </script>
